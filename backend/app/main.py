d3d665c340681833137e7d44bd0cf2d2bfe2a448
Aastik <aastikdasgupta.com>
2025-06-09 11:23:35 +0530

    CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(debug=True)
 # CORS settings
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],  # Or use ["*"] during dev
+    allow_origins=["*"],  # Or use ["*"] during dev
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
